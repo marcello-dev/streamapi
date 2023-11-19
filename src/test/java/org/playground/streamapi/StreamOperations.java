530938688adb375bc42987257848c87c20bff564
LAPTOP-287P11S9\Marc <monachesi.m@live.com>
2023-11-19 18:44:29 +0800

    format code

--- src/test/java/org/playground/streamapi/StreamOperations.java
+++ src/test/java/org/playground/streamapi/StreamOperations.java
@@ -19,16 +19,17 @@ public class StreamOperations {
     @BeforeEach
     public void setUp() {
         drivers = new ArrayList<>();
-        Driver d1 = new Driver("a","b",null);
-        d1.setInfo(new ArrayList<>(Arrays.asList("patente","libretto")));
-        Driver d2 = new Driver("c","d",new Vehicle(2));
-        d2.setInfo(new ArrayList<>(Arrays.asList("documento1","patente")));
+        Driver d1 = new Driver("a", "b", null);
+        d1.setInfo(new ArrayList<>(Arrays.asList("patente", "libretto")));
+        Driver d2 = new Driver("c", "d", new Vehicle(2));
+        d2.setInfo(new ArrayList<>(Arrays.asList("documento1", "patente")));
 
-        drivers.add(d1); drivers.add(d2);
+        drivers.add(d1);
+        drivers.add(d2);
     }
 
     @Test
-    public void matchAny_staticMethod(){
+    public void matchAny_staticMethod() {
         // Reference to a Static Method
         boolean anyRealDrivers = drivers.stream().anyMatch(Driver::isRealDriver);
 
@@ -36,40 +37,40 @@ public class StreamOperations {
     }
 
     @Test
-    public void matchAny_if_predicate(){
+    public void matchAny_if_predicate() {
         boolean anyCalledA = drivers.stream().anyMatch(d -> d.getName().equals("a"));
 
         assertTrue(anyCalledA);
     }
 
     @Test
-    public void filterArray(){
-        int[] array = {1,2,3,4,5,6,7,8};
-        int[] expected = {2,4,6,8};
+    public void filterArray() {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int[] expected = { 2, 4, 6, 8 };
 
-        int[] actual = Arrays.stream(array).filter((x)-> x%2==0).toArray();
+        int[] actual = Arrays.stream(array).filter((x) -> x % 2 == 0).toArray();
 
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void filterObjects(){
+    public void filterObjects() {
         // Reference to an Instance Method of an Object of a Particular Type
-        List<String> strings = new ArrayList<>(Arrays.asList("a","b",""));
+        List<String> strings = new ArrayList<>(Arrays.asList("a", "b", ""));
 
         long emptyStrings = strings.stream().filter(String::isEmpty).count();
 
-        assertEquals(1,emptyStrings);
+        assertEquals(1, emptyStrings);
     }
 
     @Test
     public void flatMap() {
-        List<String> expected = Arrays.asList("patente","libretto","documento1","patente");
+        List<String> expected = Arrays.asList("patente", "libretto", "documento1", "patente");
 
         // Get list of all info of the drivers
         List<String> allInfo = drivers.stream().flatMap(d -> d.getInfo().stream()).collect(Collectors.toList());
 
-        assertEquals(expected,allInfo);
+        assertEquals(expected, allInfo);
     }
 
     @Test
@@ -78,7 +79,7 @@ public class StreamOperations {
 
         Stream<Integer> actual = numbers.takeWhile(number -> number % 2 == 0);
 
-        assertArrayEquals(Stream.of(2, 4, 6, 8).toArray(),actual.toArray());
+        assertArrayEquals(Stream.of(2, 4, 6, 8).toArray(), actual.toArray());
     }
 
     @Test
@@ -87,7 +88,7 @@ public class StreamOperations {
 
         Stream<Integer> actual = numbers.dropWhile(number -> number % 2 == 0);
 
-        assertArrayEquals(Stream.of(9, 10, 12).toArray(),actual.toArray());
+        assertArrayEquals(Stream.of(9, 10, 12).toArray(), actual.toArray());
     }
 
 }
